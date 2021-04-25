ea6a9acfbc73ac7f94d12981d9a07e7f070717c9
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-04-26 04:55:58 +0800

    added geojson types

--- src/app/features/personalized-risk-assessment/services/pra.service.ts
+++ src/app/features/personalized-risk-assessment/services/pra.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FeatureCollection } from 'geojson';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HazardType, PraStore, PRAPage, RiskLevel } from '../store/pra.store';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -21,13 +21,13 @@ export class PraService {
   async assessRisk(hazardType: HazardType): Promise<void> {
     this.praStore.patch({ isLoading: true });
 
-    const response: any = await this.http
-      .get(
+    const response: FeatureCollection = await this.http
+      .get<FeatureCollection>(
         'http://localhost:8080/geoserver/wms?INFO_FORMAT=application/json&REQUEST=GetFeatureInfo&EXCEPTIONS=application/vnd.ogc.se_xml&SERVICE=WMS&VERSION=1.1.1&WIDTH=2&HEIGHT=2&X=1&Y=1&BBOX=124.55999410001472,11.580408358654404,124.56000410001472,11.580418358654404&LAYERS=cite:Biliran_Flood_100year&QUERY_LAYERS=cite:Biliran_Flood_100year&TYPENAME=cite:Biliran_Flood_100year&BUFFER=50&propertyName=Var&feature_count=50'
       )
       .toPromise();
 
-    const riskLevelNum = parseInt(response.features[0].properties.Var);
+    const riskLevelNum = parseInt(response.features[0].properties?.Var);
     let riskLevel = 'unavailable';
     switch (riskLevelNum) {
       case 0:
