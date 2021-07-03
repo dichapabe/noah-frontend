711fc21571d6919196004afb841df0d6dd0d2d77
bon <jccarpo@up.edu.ph>
2021-07-03 14:18:31 +0800

    Integrate know-your-hazards UI page to mvp2; Add search; Add map component

--- src/app/features/know-your-hazards/components/map-kyh/map-kyh.component.ts
+++ src/app/features/know-your-hazards/components/map-kyh/map-kyh.component.ts
@@ -1,4 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from '@core/services/map.service';
+import { environment } from '@env/environment';
+import { KyhService } from '@features/know-your-hazards/services/kyh.service';
+import {
+  LEYTE_FIRESTATIONS,
+  LEYTE_HOSPITALS,
+  LEYTE_POLICESTATIONS,
+  LEYTE_SCHOOLS,
+} from '@shared/mocks/critical-facilities';
+import { LEYTE_FLOOD } from '@shared/mocks/flood';
+import { LEYTE_LANDSLIDE } from '@shared/mocks/landslide';
+import { LEYTE_STORM_SURGE } from '@shared/mocks/storm-surges';
+import mapboxgl, { GeolocateControl, Map, Marker } from 'mapbox-gl';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import { fromEvent, Subject } from 'rxjs';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+import { KYHPage } from '@features/know-your-hazards/store/kyh.store';
+
+type MapStyle = 'terrain' | 'satellite';
 
 @Component({
   selector: 'noah-map-kyh',
@@ -6,7 +27,214 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-kyh.component.scss'],
 })
 export class MapKyhComponent implements OnInit {
-  constructor() {}
+  map!: Map;
+  geolocateControl!: GeolocateControl;
+  centerMarker!: Marker;
+  mapStyle: MapStyle = 'terrain';
 
-  ngOnInit(): void {}
+  private _unsub = new Subject();
+
+  constructor(private mapService: MapService, private kyhService: KyhService) {}
+
+  ngOnInit(): void {
+    this.initMap();
+
+    fromEvent(this.map, 'load')
+      .pipe(takeUntil(this._unsub))
+      .subscribe(() => {
+        this.initGeocoder();
+        this.initGeolocation();
+
+        this.initPageListener();
+        this.initCenterListener();
+        this.initGeolocationListener();
+      });
+
+    fromEvent(this.map, 'style.load')
+      .pipe(takeUntil(this._unsub))
+      .subscribe(() => {
+        this.initLayers();
+        this.initMarkers();
+        this.hideAllLayers();
+
+        const page = this.kyhService.currentPage;
+        this.showLayers(page);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._unsub.next();
+    this._unsub.complete();
+  }
+
+  hideAllLayers() {
+    this.kyhService.hazardTypes.forEach((hazard) => {
+      this.map.setLayoutProperty(hazard, 'visibility', 'none');
+    });
+  }
+
+  initCenterListener() {
+    this.kyhService.center$
+      .pipe(distinctUntilChanged(), takeUntil(this._unsub))
+      .subscribe((center) => {
+        this.map.flyTo({
+          center,
+          zoom: 15,
+          essential: true,
+        });
+      });
+  }
+
+  initGeocoder() {
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: {
+        color: 'orange',
+      },
+      flyTo: {
+        padding: 15, // If you want some minimum space around your result
+        easing: function (t) {
+          return t;
+        },
+        maxZoom: 13, // If you want your result not to go further than a specific zoom
+      },
+    });
+    this.map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    geocoder.on('result', (e) => {
+      this.kyhService.setCurrentLocation(e.result['place_name']);
+      console.log(e.result);
+    });
+  }
+
+  initGeolocation() {
+    this.geolocateControl = this.mapService.getNewGeolocateControl();
+    this.map.addControl(this.geolocateControl, 'top-right');
+  }
+
+  initGeolocationListener() {
+    const _this = this;
+
+    fromEvent(this.geolocateControl, 'geolocate')
+      .pipe(takeUntil(this._unsub), debounceTime(2500))
+      .subscribe(locateUser);
+
+    async function locateUser(e) {
+      try {
+        const { latitude, longitude } = e.coords;
+        const myPlace = await _this.mapService.reverseGeocode(
+          latitude,
+          longitude
+        );
+        _this.kyhService.setCurrentLocation(myPlace);
+      } catch (error) {
+        // temporary
+        alert(
+          'Unable to retrieve your location, please manually input your city / brgy'
+        );
+      }
+    }
+  }
+
+  initLayers() {
+    this.map.addLayer(LEYTE_FLOOD);
+    this.map.addLayer(LEYTE_LANDSLIDE);
+    this.map.addLayer(LEYTE_STORM_SURGE);
+  }
+
+  initPageListener() {
+    this.kyhService.currentPage$
+      .pipe(takeUntil(this._unsub))
+      .subscribe((page) => {
+        this.showLayers(page);
+      });
+  }
+
+  initMap() {
+    this.mapService.init();
+    this.map = new mapboxgl.Map({
+      container: 'map-kyh',
+      style: environment.mapbox.styles[this.mapStyle],
+      zoom: 13,
+      pitch: 50,
+      touchZoomRotate: true,
+      bearing: 30,
+      center: this.kyhService.currentCoords,
+    });
+  }
+
+  initMarkers() {
+    this.centerMarker = new mapboxgl.Marker({ color: '#333' })
+      .setLngLat(this.kyhService.currentCoords)
+      .addTo(this.map);
+
+    this.kyhService.currentCoords$
+      .pipe(takeUntil(this._unsub))
+      .subscribe((currentCoords) => {
+        this.centerMarker.setLngLat(currentCoords);
+      });
+
+    const _this = this;
+    this.map.loadImage('assets/map-sprites/hospital.png', (error, image) => {
+      if (error) throw error;
+      _this.map.addImage('icon-hospital', image);
+      _this.map.addLayer(LEYTE_HOSPITALS);
+    });
+
+    this.map.loadImage(
+      'assets/map-sprites/fire-station.png',
+      (error, image) => {
+        if (error) throw error;
+        _this.map.addImage('icon-firestation', image);
+        _this.map.addLayer(LEYTE_FIRESTATIONS);
+      }
+    );
+
+    this.map.loadImage(
+      'assets/map-sprites/police-station.png',
+      (error, image) => {
+        if (error) throw error;
+        _this.map.addImage('icon-policestation', image);
+        _this.map.addLayer(LEYTE_POLICESTATIONS);
+      }
+    );
+
+    this.map.loadImage('assets/map-sprites/school.png', (error, image) => {
+      if (error) throw error;
+      _this.map.addImage('icon-school', image);
+      _this.map.addLayer(LEYTE_SCHOOLS);
+    });
+  }
+
+  showAllLayers() {
+    this.kyhService.hazardTypes.forEach((hazard) => {
+      this.map.setLayoutProperty(hazard, 'visibility', 'visible');
+    });
+  }
+
+  showCurrentHazardLayer(page: KYHPage) {
+    if (!this.kyhService.isHazardPage(page)) return;
+
+    this.map.setLayoutProperty(page, 'visibility', 'visible');
+  }
+
+  showLayers(page: KYHPage) {
+    if (page === 'know-your-hazards') {
+      this.showAllLayers();
+      return;
+    }
+
+    this.hideAllLayers();
+    this.showCurrentHazardLayer(page);
+  }
+
+  switchMapStyle(style: MapStyle) {
+    if (this.mapStyle === style) return;
+
+    if (style in environment.mapbox.styles) {
+      this.mapStyle = style;
+      this.map.setStyle(environment.mapbox.styles[style]);
+    }
+  }
 }
--- src/app/features/know-your-hazards/pages/know-your-hazards/know-your-hazards.component.ts
+++ src/app/features/know-your-hazards/pages/know-your-hazards/know-your-hazards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { KyhService } from '@features/know-your-hazards/services/kyh.service';
+import { RiskLevel } from '@features/know-your-hazards/store/kyh.store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'noah-know-your-hazards',
@@ -6,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./know-your-hazards.component.scss'],
 })
 export class KnowYourHazardsComponent implements OnInit {
-  constructor() {}
+  searchTerm: string;
+  currentLocation$: Observable<string>;
+  riskLevel$: Observable<RiskLevel>;
+  isFlood: boolean = false;
+  islandSlide: boolean = false;
+  isStorm: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(private kyhService: KyhService) {
+    this.riskLevel$ = this.kyhService.riskLevel$;
+  }
+
+  ngOnInit(): void {
+    this.kyhService.init();
+    this.kyhService.setCurrentPage('know-your-hazards');
+    this.currentLocation$ = this.kyhService.currentLocation$;
+  }
+  selectPlace(selectedPlace) {
+    this.kyhService.setCurrentLocation(selectedPlace.text);
+    const [lng, lat] = selectedPlace.center;
+    this.kyhService.setCenter({ lat, lng });
+    this.kyhService.setCurrentCoords({ lat, lng });
+  }
 }
