98eef2a46cd78551c746b200f9ffee424faa4131
bon <jccarpo@up.edu.ph>
2021-07-15 10:24:01 +0800

    add separate component for hazards info survival kit

---
+++ src/app/features/know-your-hazards/components/survival-kit/survival-kit.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SurvivalKitComponent } from './survival-kit.component';
+
+describe('SurvivalKitComponent', () => {
+  let component: SurvivalKitComponent;
+  let fixture: ComponentFixture<SurvivalKitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SurvivalKitComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SurvivalKitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/know-your-hazards/components/survival-kit/survival-kit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-survival-kit',
+  templateUrl: './survival-kit.component.html',
+  styleUrls: ['./survival-kit.component.scss'],
+})
+export class SurvivalKitComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
