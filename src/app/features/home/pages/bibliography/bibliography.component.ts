10fdeaa9502f596a19961f91da1fc6e9f43a67cd
kennethbeoliporada <kennethbeoliporada@gmail.com>
2021-10-01 08:38:54 +0800

    WIP: change caption bibliography and link to playground

---
+++ src/app/features/home/pages/bibliography/bibliography.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-bibliography',
+  templateUrl: './bibliography.component.html',
+  styleUrls: ['./bibliography.component.scss'],
+})
+export class BibliographyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
