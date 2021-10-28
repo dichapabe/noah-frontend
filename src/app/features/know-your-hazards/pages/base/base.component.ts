573d166fda43c88094b1a3aabae4e594868ad2b2
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-10-28 19:33:15 +0800

    add search

--- src/app/features/know-your-hazards/pages/base/base.component.ts
+++ src/app/features/know-your-hazards/pages/base/base.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { KyhService } from '@features/know-your-hazards/services/kyh.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'noah-base',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base.component.scss'],
 })
 export class BaseComponent implements OnInit {
-  constructor() {}
+  currentLocation$: Observable<string>;
+
+  constructor(private kyhService: KyhService) {}
 
   ngOnInit(): void {}
+
+  selectPlace(selectedPlace) {
+    this.kyhService.setCurrentLocation(selectedPlace.text);
+    const [lng, lat] = selectedPlace.center;
+    this.kyhService.setCenter({ lat, lng });
+    this.kyhService.setCurrentCoords({ lat, lng });
+  }
 }
